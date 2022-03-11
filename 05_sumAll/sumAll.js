0aa47ab65a58a524b103086db1394ceac592843a
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-11 17:29:46 -0500

    Finish sumAll.js excersice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
+    if ( typeof first != "number" || typeof second != "number" || first < 0 || second < 0){
+        return "ERROR"
+    }
+
+    const largerNumber = Math.max(first, second)
+    var smallerNumber = Math.min(first,second)
+    var sum = 0
+
+    for(smallerNumber; smallerNumber <= largerNumber; smallerNumber++){
+        sum += smallerNumber
+    }
+    return sum
 };
+
+console.log(sumAll(1, 5))
 
 // Do not edit below this line
 module.exports = sumAll;
