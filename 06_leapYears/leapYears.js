1235624288ed47ff1ea43133da31d748b4ea17df
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-11 18:19:23 -0500

    Finish LeapYears Excersice

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,6 @@
 const leapYears = function(year) {
-  var result= false
-   if (year % 4 == 0){
-        result = true
-       if(year % 100 == 0 && year % 400 != 0){
-        result = false
-       }
-  
-   } else{
-    result = false
-   }
 
-   return result
+   return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
 };
 
 // Do not edit below this line
