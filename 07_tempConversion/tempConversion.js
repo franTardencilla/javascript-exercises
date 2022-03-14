bcbc5ddc18ba7cdb97fc6176e1924d58c8e050a1
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-14 15:54:58 -0400

    Finish TempCoversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
+const ftoc = function (temp) {
 
+  const newTemp =  ((temp - 32) * (5/9))
+
+  console.log(newTemp)
+  return (newTemp % 1 == 0) ? newTemp : parseFloat(newTemp.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const newTemp = ((temp * (9/5)) + 32)
+  return (newTemp % 1 == 0) ? newTemp : parseFloat(newTemp.toFixed(1))
 };
+
+console.log(ftoc(100))
 
 // Do not edit below this line
 module.exports = {
