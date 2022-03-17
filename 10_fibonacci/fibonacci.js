d3da01e9a46233a3b470b933a91ddf3ab0efd342
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-17 16:20:03 -0400

    Finish Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if(number < 0) return "OOPS"
+    
+    let count = [1,1];
+    for(let i = 2; i<number; i++){
+        count[i] = count[i - 1] + count[i - 2]
+    }
+
+    return count[number - 1]
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
