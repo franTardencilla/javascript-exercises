03418d98494bfe9c91d7919191378174f86ed147
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-17 14:06:49 -0400

    Finish calculator excersise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	return numbers.reduce((total, number) =>{
+    return total + number
+  }, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  return numbers.reduce((total, number) =>{
+    return Math.abs(number - total)
+  }, 0)
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) =>{
+    return parseInt( total + number)
+  }, 0)
 };
 
-const multiply = function() {
 
+const multiply = function(numbers) {
+  return numbers.reduce((total, number) => {
+    return total * number
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent){
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+if(number === 0) return 1
+
+	let sum = 1
+  for(let i = 1; i <= number; i++){
+    sum *= i
+  }
+  return sum
 };
 
 // Do not edit below this line
