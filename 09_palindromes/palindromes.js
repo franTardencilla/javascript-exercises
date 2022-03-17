982e9420a28a7f1a31ac2c9867deec45db142030
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-17 15:07:55 -0400

    Finish Palindrome Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    let regex = /[^a-z]/gi
+ 
+    const base = sentence.replace(regex,"").toLowerCase()
+    console.log(base)
+    return base.split("").reverse().join("") === base
 };
 
 // Do not edit below this line
